22400aecea97d378581c1a85e6cd0f18f57587b2
hyungju19 <hyungju911@gamil.com>
2023-12-18 11:07:20 +0900

    Initial commit

---
+++ src/main/resources/static/js/list.js
@@ -0,0 +1,11 @@
+
+$(function(){
+   $("[name='pageRows']").change(function (){
+       let frm = $("[name='frmPageRows']");
+       frm.attr("method","POST")
+       frm.attr("action","pageRows")
+       frm.submit();
+   });
+
+
+});
